9d3b996281751eb1f2cab7d1b0963d7a136690ad
Tang4109 <2298964704@qq.com>
2019-08-16 11:56:19 +0800

    add some annotations

--- 2.4.1_decision_function.py
+++ 2.4.1_decision_function.py
@@ -6,7 +6,9 @@ from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.model_selection import train_test_split
 
 X, y = make_circles(noise=0.25, factor=0.5, random_state=1)
+# 类别重命名
 y_named = np.array(['blue', 'red'])[y]
+#训练
 X_train, X_test, y_train_named, y_test_named, y_train, y_test = train_test_split(X, y_named, y, random_state=0)
 gbrt = GradientBoostingClassifier(random_state=0)
 gbrt.fit(X_train, y_train_named)
@@ -20,9 +22,9 @@ print(np.all(gbrt.classes_[greater_zero] == pred))
 decision_function = gbrt.decision_function(X_test)
 print(np.min(decision_function))
 print(np.max(decision_function))
+
 # 画图
 fig, axes = plt.subplots(1, 2, figsize=(13, 5))  # 创建一张画布并且创建子图
-
 mglearn.tools.plot_2d_separator(gbrt, X, ax=axes[0], alpha=0.4, fill=True, cm=mglearn.cm2)
 scores_image = mglearn.tools.plot_2d_scores(gbrt, X, ax=axes[1], alpha=0.4, cm=mglearn.ReBl)
 for ax in axes:
@@ -31,5 +33,5 @@ for ax in axes:
     ax.set_xlabel('Feature 0')
     ax.set_ylabel('Feature 1')
 cbar = plt.colorbar(scores_image, ax=axes.tolist())  # 颜色棒，颜色越深代表置信度越高
-axes[0].legend(['Test class 0','Test class 1','Train class 0','Train class 1'],ncol=4,loc=(0.1,1.1))
+axes[0].legend(['Test class 0', 'Test class 1', 'Train class 0', 'Train class 1'], ncol=4, loc=(0.1, 1.1))
 plt.show()
