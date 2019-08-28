37a62ff80b6f928ff8d2eb3fb521ed54000c4cf6
Tang4109 <2298964704@qq.com>
2019-08-28 22:44:36 +0800

    数据表示与特征工程

--- 4.1.2_OneHotEncoder.py
+++ 4.1.2_OneHotEncoder.py
@@ -1,7 +1,12 @@
 import pandas as pd
+pd.set_option('display.max_rows', None)  # 可以填数字，填None表示'行'无限制
+pd.set_option('display.max_columns', None) # 可以填数字，填None表示'列'无限制
 from IPython.display import display
 
 demo_df = pd.DataFrame({'Integer Feature': [0, 1, 2, 1],
                         'Categorical Feature': ['socks', 'fox', 'socks', 'box']})
 
 display(demo_df)
+
+dummies_demo_df = pd.get_dummies(demo_df,columns=['Integer Feature','Categorical Feature'])
+print(dummies_demo_df)
