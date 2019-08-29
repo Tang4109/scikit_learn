c427085ed5d2bd3ae10d2b0f5f08f833e118766b
Tang4109 <2298964704@qq.com>
2019-08-29 09:53:11 +0800

    分箱

--- 4.2_binning.py
+++ 4.2_binning.py
@@ -9,17 +9,17 @@ X, y = mglearn.datasets.make_wave(n_samples=100)
 
 line = np.linspace(-3, 3, 1000, endpoint=False).reshape(-1, 1)  # reshape(-1, 1)让line变成只有一列，行数自动计算
 # 如果是真，则一定包括stop，如果为False，一定不会有stop
-# print(line)
-# reg = DecisionTreeRegressor(min_samples_split=3).fit(X, y)
-# plt.plot(line, reg.predict(line), label='decision tree')
-# reg = LinearRegression().fit(X, y)
-# plt.plot(line, reg.predict(line), label='linear regression')
-#
-# plt.plot(X[:, 0], y, 'o', c='k')
-# plt.ylabel('regression output')
-# plt.xlabel('input feature')
-# plt.legend(loc='best')
-# plt.show()
+print(line)
+reg = DecisionTreeRegressor(min_samples_split=3).fit(X, y)
+plt.plot(line, reg.predict(line), label='decision tree')
+reg = LinearRegression().fit(X, y)
+plt.plot(line, reg.predict(line), label='linear regression')
+
+plt.plot(X[:, 0], y, 'o', c='k')
+plt.ylabel('regression output')
+plt.xlabel('input feature')
+plt.legend(loc='best')
+plt.show()
 
 bins = np.linspace(-3, 3, 11)
 which_bin = np.digitize(X, bins)
